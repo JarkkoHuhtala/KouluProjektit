0fcfde93df1f07faadf98d602f17da34443ba249
jarkkohuhtala <huhtalajarkko2@gmail.com>
2026-01-07 17:37:08 +0200

    Ekan tunnin tehtävät

--- Plain_Cplus/main.cpp
+++ Plain_Cplus/main.cpp
@@ -1,10 +1,53 @@
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 
+int randNum = 0;
+int userInput = 0;
+int guesses = 0;
+bool randFlag = false;
+bool startFlag = true;
+
+int game(int maxNum)
+{
+    if(randFlag != true)
+    {
+        srand (time(NULL));
+        randNum = rand() % maxNum;
+        randFlag = true;
+    }
+
+    cout<<"Guess the number\n";
+    cin>>userInput;
+
+    if(userInput > randNum)
+    {
+        cout<<"Guess was too high\n";
+        guesses++;
+        return guesses;
+    }else if(userInput < randNum)
+    {
+        cout<<"Guess was too low\n";
+        guesses++;
+        return guesses;
+    }else if(userInput == randNum)
+    {
+        cout<<"You got it, the number was ";
+        guesses++;
+        startFlag = false;
+        return guesses;
+    }
+
+}
+
 int main()
 {
-    cout << "Hello World!\n\n";
-    cout << "brah moment";
-    return 0;
+    while(startFlag != false)
+    {
+        int guesses2 = 0;
+        guesses2 = game(100);
+        cout<<"Guesses "<<guesses2<<"\n";
+    }
 }
